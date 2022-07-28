abd203c392b47a7dc2d84ae894e8411611077686
Beso Poladishvili <beskapoladishvili@gmail.com>
2022-07-28 17:49:14 +0400

    add click handler functionality to input element to open close close the dropdow

--- select/select.js
+++ select/select.js
@@ -1,6 +1,6 @@
 const getTemplate = () => {
   return /*html*/`
-  <div class="select__input">
+  <div class="select__input" data-type="input">
     <span>Heloo</span>
     <i class="fa-solid fa-chevron-down"></i>
   </div>
@@ -25,15 +25,40 @@ export class Select {
   constructor(selector, options) {
     this.$el = document.querySelector(selector)
     this.#render()
+    this.#setup()
   }
 
   #render() {
     this.$el.innerHTML = getTemplate()
     this.$el.classList.add('select')
   }
+
+  #setup() {
+    this.clickHandler = this.clickHandler.bind(this)
+
+    this.$el.addEventListener('click', this.clickHandler)
+  }
+
+  clickHandler(event) {
+    const { type } = event.target.dataset
+
+    if (type === "input") {
+      this.toggle()
+    }
+  }
+
+  get isOpen() {
+    return this.$el.classList.contains('open')
+  }
+
+  toggle() {
+    this.isOpen ? this.close() : this.open()
+  }
+
   open() {
     this.$el.classList.add('open')
   }
+
   close() {
     this.$el.classList.remove('open')
   }
