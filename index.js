fa5097f70ab4587dee601d160bea6338e466e42d
Beso Poladishvili <beskapoladishvili@gmail.com>
2022-07-28 13:43:10 +0400

    add dropdown and input elements to html doc and styled a bit

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-const button = document.getElementsByTagName('button')[0]
-button.addEventListener('click', (e) => {
-  console.log(e.target)
-})
-
-
-
-const idToUserMap = {
-  123: {
-    firstName: '',
-    lastName: 'John',
-    age: 30
-  }
-}
-
-console.log(idToUserMap[123]?.firstName?.substring(0, 1))
+import './style.scss'
